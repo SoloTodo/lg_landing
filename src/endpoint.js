bb57a32cf89ded83e0b036261ccbe34fbc5dca96
David <david@fml.cl>
2020-06-11 13:45:19 -0400

    Changed endpoint to use api

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,4 +1,4 @@
-// export const endpoint = 'https://publicapi.solotodo.com/';
+export const endpoint = 'https://publicapi.solotodo.com/';
 // export const endpoint = 'https://testapi.solotodo.com/';
-export const endpoint = 'http://localhost:8000/';
+//export const endpoint = 'http://localhost:8000/';
 // export const endpoint = 'http://192.168.0.187:8000/';
