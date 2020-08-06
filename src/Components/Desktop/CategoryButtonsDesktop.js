6eb52ecca807ea73a0f205f2f7fd98d37c2634a6
David <david@fml.cl>
2020-08-06 17:13:06 -0400

    ADDED: Category Buttons Desktop now working

--- src/Components/Desktop/CategoryButtonsDesktop.js
+++ src/Components/Desktop/CategoryButtonsDesktop.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import {
+    UncontrolledButtonDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from "reactstrap"
+import classNames from "classnames"
 
 import navigation from "../../Layout/_nav";
+import CategoryLink from "../CategoryLink";
 
 
 class CategoryButtonsDesktop extends React.Component {
     render() {
         const pathname = this.props.location.pathname;
-        let initialSlide = 0;
+        let selectedButton = navigation.items[0].name;
 
         for (const idx in navigation.items) {
             if (navigation.items[idx].url === pathname) {
-                initialSlide = idx;
+                selectedButton = navigation.items[idx].name;
             }
         }
 
@@ -19,7 +27,55 @@ class CategoryButtonsDesktop extends React.Component {
             return item.button
         })
 
-        return <div>Category Buttons Desktop Placeholder</div>
+        const singleItems = items.filter(item =>
+            !item.button_parent_name
+        )
+
+        const multiItems = items.filter(item => item.button_parent_name)
+        const groupedMultiItems = {}
+
+        for (const item of multiItems) {
+            if (!groupedMultiItems[item.button_parent_name]){
+                groupedMultiItems[item.button_parent_name] = []
+            }
+            groupedMultiItems[item.button_parent_name].push(item)
+        }
+
+
+        return <div className="d-flex justify-content-between ml-5 mr-5 mt-5 mb-5">
+            {
+                singleItems.map(item => {
+                    const isSelected = item.name === selectedButton
+                    return <CategoryLink
+                        to={item.url}
+                        className={classNames('desktop-category-button d-flex justify-content-center align-items-center', {selected:isSelected})} key={item.button_name}>
+                        {item.button_name}
+                    </CategoryLink>
+                })
+            }
+            {
+                Object.entries(groupedMultiItems).map(itemList => {
+                    let isSelected = false
+                    for (const item of itemList[1]) {
+                        if (item.name === selectedButton) {
+                            isSelected = true;
+                        }
+                    }
+                    return <UncontrolledButtonDropdown key={itemList[0]}>
+                        <DropdownToggle caret className={classNames('desktop-category-button d-flex justify-content-center align-items-center', {selected:isSelected})}>
+                            {itemList[0]}
+                        </DropdownToggle>
+                        <DropdownMenu>
+                        {itemList[1].map(item => {
+                            return <DropdownItem key={item.button_name}>
+                                <CategoryLink to={item.url}>{item.button_name}</CategoryLink>
+                            </DropdownItem>
+                        })}
+                        </DropdownMenu>
+                    </UncontrolledButtonDropdown>
+                })
+            }
+        </div>
     }
 }
 
--- src/Layout/_nav.js
+++ src/Layout/_nav.js
@@ -16,6 +16,7 @@ export default {
             name: 'LAVADORAS',
             button: true,
             button_name: 'LAVADORAS',
+            button_parent_name: 'LINEA BLANCA',
             url: '/lavadoras',
         },
         {
@@ -28,18 +29,21 @@ export default {
             name: 'REFRIGERADORES',
             button: true,
             button_name: 'REFRIGERADORES',
+            button_parent_name: 'LINEA BLANCA',
             url: '/refrigeradores'
         },
         {
             name: 'MONITORES',
             button: true,
             button_name: 'MONITORES',
+            button_parent_name: 'COMPUTACIÓN',
             url: '/monitores'
         },
         {
             name: 'PROYECTORES',
             button: true,
             button_name: 'PROYECTORES',
+            button_parent_name: 'COMPUTACIÓN',
             url: '/proyectores'
         }
     ]
