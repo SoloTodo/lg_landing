353ec642f326be5d74227ba223b0366cd65d2390
Vijay Khemlani <vkhemlan@gmail.com>
2020-08-10 16:15:57 -0400

    * Changed cookie policy to blank target

--- src/Components/CookiesMessage.js
+++ src/Components/CookiesMessage.js
@@ -13,7 +13,10 @@ class CookiesMessage extends React.Component {
     }
 
     onAcceptClick = () => {
-        setCookie([], 'cookiesAgree', 'True', []);
+        setCookie([], 'cookiesAgree', 'True', {
+            maxAge: 30 * 24 * 60 * 60,
+            path: '/',
+        });
         this.setState({
             accepted: true
         })
@@ -26,7 +29,7 @@ class CookiesMessage extends React.Component {
 
         return <Container fluid id="cookiesMessage" className="cookies-message">
             <p className="cookies-message-text">Este sitio web recopila cookies para brindarle una experiencia mejorada y personalizada. Las cookies recopiladas no se utilizarán para ningún otro propósito que no sea el descrito en nuestras políticas.<br/>
-                Al continuar navegando por el sitio, acepta nuestro uso de cookies. Consulte nuestra Política de privacidad y cookies para obtener más información. <a href="https://www.lg.com/cl/privacy" className="cookies-message-link">Ver nuestra Politica de Privacidad y uso de Cookie.</a></p>
+                Al continuar navegando por el sitio, acepta nuestro uso de cookies. Consulte nuestra Política de privacidad y cookies para obtener más información. <a href="https://www.lg.com/cl/privacy" className="cookies-message-link" target="_blank" rel="noopener noreferrer">Ver nuestra Politica de Privacidad y uso de Cookie.</a></p>
             <Button close className="cookies-message-accept" onClick={this.onAcceptClick}/>
         </Container>
     }
