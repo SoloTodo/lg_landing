5f65a7d0f67b09914b8417fcb7f267c324a4b3cd
David <david@fml.cl>
2020-08-10 16:03:27 -0400

    ADDED: Privacy policy link to cookie message

--- src/Components/CookiesMessage.js
+++ src/Components/CookiesMessage.js
@@ -26,7 +26,7 @@ class CookiesMessage extends React.Component {
 
         return <Container fluid id="cookiesMessage" className="cookies-message">
             <p className="cookies-message-text">Este sitio web recopila cookies para brindarle una experiencia mejorada y personalizada. Las cookies recopiladas no se utilizarán para ningún otro propósito que no sea el descrito en nuestras políticas.<br/>
-                Al continuar navegando por el sitio, acepta nuestro uso de cookies. Consulte nuestra Política de privacidad y cookies para obtener más información. Ver nuestra Politica de Privacidad y uso de Cookie.</p>
+                Al continuar navegando por el sitio, acepta nuestro uso de cookies. Consulte nuestra Política de privacidad y cookies para obtener más información. <a href="https://www.lg.com/cl/privacy" className="cookies-message-link">Ver nuestra Politica de Privacidad y uso de Cookie.</a></p>
             <Button close className="cookies-message-accept" onClick={this.onAcceptClick}/>
         </Container>
     }
